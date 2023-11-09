58b73e40542e417a2f99c4602614ead30ba21110
vertesy <abel.vertesy@imba.oeaw.ac.at>
2023-11-09 18:00:17 +0100

    test checkVars

--- tests/testthat/test-isoENV.R
+++ tests/testthat/test-isoENV.R
@@ -1,6 +1,6 @@
 library(testthat)
 
-
+# removeAllExceptFunctions -------------------------------------------------------------------------
 
 # Test that functions are not removed
 test_that("Functions are not removed", {
@@ -34,4 +34,51 @@ test_that("Variables are removed from specified environment", {
   # Clean up
   rm(test_env)
 })
+
+
+test_env <- new.env()
+test_env$var_defined <- 1
+test_env$var_null <- NULL
+test_env$var_na <- NA
+test_env$var_nan <- as.numeric(NaN)
+test_env$var_inf <- Inf
+test_env$var_empty <- numeric(0)
+
+# checkVars ----------------------------------------------------------------------------------------
+
+test_that("checkVars issues warnings appropriately", {
+  output_variables <- c("var_defined", "var_null", "var_na",
+                        "var_nan", "var_inf", "var_empty", "var_missing")
+
+  # Capture the warnings
+  expect_warning(checkVars(output_variables, test_env), "var_null is NULL")
+  expect_warning(checkVars(output_variables, test_env), "var_na is NA")
+  expect_warning(checkVars(output_variables, test_env), "var_nan is NaN")
+  expect_warning(checkVars(output_variables, test_env), "var_inf is Inf")
+  expect_warning(checkVars(output_variables, test_env), "var_empty is empty")
+  expect_warning(checkVars(output_variables, test_env), "var_missing is missing")
+})
+
+test_that("checkVars does not issue a message for defined variables", {
+  output_variables <- c("var_defined")
+
+  # Capture the message
+  expect_message(checkVars(output_variables, test_env), "var_defined is defined and not empty")
+})
+
+
+
+# xxx -------------------------------------------------------------------------
+
+
+
+# xxx -------------------------------------------------------------------------
+
+
+
+# xxx -------------------------------------------------------------------------
+
+
+
+# xxx -------------------------------------------------------------------------
 

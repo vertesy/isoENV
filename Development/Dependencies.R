edc007defcc9afd8f78d8c8fc0d4cd4134bb0402
vertesy <abel.vertesy@imba.oeaw.ac.at>
2023-11-27 11:32:43 +0100

    dep

--- Development/Dependencies.R
+++ Development/Dependencies.R
@@ -1,4 +1,4 @@
-Dependency file generated on Mon Nov 27 11:22:52 2023 
+Dependency file generated on Mon Nov 27 11:32:09 2023 
 
 #################################################################################################### 
 isoENV.other.R
@@ -14,20 +14,35 @@ utils
 #################################################################################################### 
 isoENV.R
 #################################################################################################### 
+$`character(0)`
+[1] "findGlobals"
+
 $`package:base`
- [1] "any"            "as.character"   "assign"        
- [4] "baseenv"        "basename"       "cat"           
- [7] "exists"         "Filter"         "format"        
-[10] "get"            "globalenv"      "identical"     
-[13] "is.character"   "is.environment" "is.function"   
-[16] "is.infinite"    "is.nan"         "is.null"       
-[19] "is.numeric"     "isFALSE"        "length"        
-[22] "list2env"       "ls"             "message"       
-[25] "mget"           "names"          "Negate"        
-[28] "new.env"        "paste"          "paste0"        
-[31] "return"         "rm"             "sapply"        
-[34] "source"         "stop"           "stopifnot"     
-[37] "substitute"     "warning"       
+ [1] "any"              "as.character"    
+ [3] "as.environment"   "assign"          
+ [5] "baseenv"          "basename"        
+ [7] "c"                "cat"             
+ [9] "deparse"          "do.call"         
+[11] "eval"             "exists"          
+[13] "Filter"           "format"          
+[15] "get"              "globalenv"       
+[17] "identical"        "invisible"       
+[19] "is.character"     "is.environment"  
+[21] "is.function"      "is.infinite"     
+[23] "is.logical"       "is.nan"          
+[25] "is.null"          "is.numeric"      
+[27] "isFALSE"          "length"          
+[29] "list"             "list2env"        
+[31] "logical"          "ls"              
+[33] "message"          "mget"            
+[35] "names"            "Negate"          
+[37] "new.env"          "parse"           
+[39] "paste"            "paste0"          
+[41] "requireNamespace" "return"          
+[43] "rm"               "sapply"          
+[45] "source"           "stop"            
+[47] "stopifnot"        "substitute"      
+[49] "vapply"           "warning"         
 
 $`package:checkmate`
 [1] "anyMissing"
@@ -38,6 +53,7 @@ $`package:isoENV`
 $`package:utils`
 [1] "lsf.str"     "object.size"
 
+character(0)
 base
 checkmate
 isoENV

c030bb72713191db8e5e075159f6e724da77af49
vertesy <abel.vertesy@imba.oeaw.ac.at>
2023-11-27 11:23:03 +0100

    dep

--- Development/Dependencies.R
+++ Development/Dependencies.R
@@ -1,20 +1,42 @@
+Dependency file generated on Mon Nov 27 11:22:52 2023 
+
+#################################################################################################### 
+isoENV.other.R
+#################################################################################################### 
 $`package:base`
- [1] "all"            "any"            "assign"         "baseenv"        "basename"      
- [6] "cat"            "exists"         "Filter"         "globalenv"      "identical"     
-[11] "is.character"   "is.environment" "is.infinite"    "is.nan"         "is.null"       
-[16] "length"         "list2env"       "ls"             "message"        "mget"          
-[21] "Negate"         "new.env"        "paste0"         "rm"             "sapply"        
-[26] "setdiff"        "source"         "stop"           "stopifnot"      "substitute"    
-[31] "warning"       
+[1] "ls"      "rm"      "setdiff"
+
+$`package:utils`
+[1] "lsf.str"
+
+base
+utils
+#################################################################################################### 
+isoENV.R
+#################################################################################################### 
+$`package:base`
+ [1] "any"            "as.character"   "assign"        
+ [4] "baseenv"        "basename"       "cat"           
+ [7] "exists"         "Filter"         "format"        
+[10] "get"            "globalenv"      "identical"     
+[13] "is.character"   "is.environment" "is.function"   
+[16] "is.infinite"    "is.nan"         "is.null"       
+[19] "is.numeric"     "isFALSE"        "length"        
+[22] "list2env"       "ls"             "message"       
+[25] "mget"           "names"          "Negate"        
+[28] "new.env"        "paste"          "paste0"        
+[31] "return"         "rm"             "sapply"        
+[34] "source"         "stop"           "stopifnot"     
+[37] "substitute"     "warning"       
 
 $`package:checkmate`
 [1] "anyMissing"
 
 $`package:isoENV`
-[1] "checkVars"
+[1] ".removeBigObjsFromEnv" "checkVars"            
 
 $`package:utils`
-[1] "lsf.str"
+[1] "lsf.str"     "object.size"
 
 base
 checkmate
